104aab557f723e7a66a12896b0f2aef2a0ac6c2b
Lucas Confalonieri <lucasconfa7.lc@gmail.com>
2022-06-14 23:03:47 -0300

    interface contacto

--- gestorDeEmail/interfaces/contacto.interface.ts
+++ gestorDeEmail/interfaces/contacto.interface.ts
@@ -1,5 +1,9 @@
 import { Email } from "../src/email";
 
 export interface Contacto{
+    nombre: string;
+    correo: string;
+    BandejaEnviados: Email[];
+    BandejaEntrada: Email[];
     update(email: Email): void;
 }
